d3d462db85fad12ef745515027f07df3aefca435
Chandu <chandru9052@gmail.com>
2024-02-15 17:48:34 +0530

    [chandu] Implemented the Coravel approach

---
+++ coravel-demo/DL/FetchDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace coravel_demo.DL
+{
+	public class FetchDetails
+	{
+		private static readonly string[] Summaries = new[]
+{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+		public IEnumerable<WeatherForecast> GetWeatherForecast()
+		{
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+	}
+}
---
+++ coravel-demo/Schedulers/AppHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Coravel.Invocable;
+using coravel_demo.DL;
+
+namespace coravel_demo.Schedulers
+{
+	public class AppHealthCheck : IInvocable
+	{
+		public Task Invoke()
+		{
+			var result = new FetchDetails().GetWeatherForecast();
+			Console.WriteLine($"Sample Invocable ran! {result.FirstOrDefault().TemperatureC}");
+			return Task.CompletedTask;
+		}
+	}
+}
